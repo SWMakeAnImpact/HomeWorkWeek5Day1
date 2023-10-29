76258d5b93e830789046bfbd955f7ef142960f2e
SWMakeAnImpact <wilkessarah96@gmail.com>
2023-10-29 18:25:21 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Task 3 Menu Data Structure
+var menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+// Task 1
+let mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+mainEl.classList.add('flex-ctr');
+
+// Task 2
+let topMenuEl = document.getElementById('top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
+
+// Task 3.1
+menuLinks.forEach(function (link) {
+  let a = document.createElement('a');
+  a.href = link.href;
+  a.textContent = link.text;
+  topMenuEl.appendChild(a);
+});
